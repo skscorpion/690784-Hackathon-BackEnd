7d977e39f4fb74cd09badf7db2f68efe043b92c8
senthil kumar <senthilkmaar@gmail.com>
2019-08-05 22:55:09 +0530

    final update

---
+++ Outreach.Reporting.Api/Controllers/AssociateController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Outreach.Reporting.Business.Interfaces;
+using Outreach.Reporting.Entity.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Outreach.Reporting.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin, PMO, POC")]
+    public class AssociateController : ControllerBase
+    {
+        private readonly IAssociateProcessor _associateProcessor;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(AssociateController));
+
+        public AssociateController(IAssociateProcessor associateProcessor)
+        {
+            _associateProcessor = associateProcessor;
+        }
+        // GET api/AllAssociates
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            log.Info("Hello logging world!");
+            var result = await _associateProcessor.GetAll();
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] List<Associate> associates)
+        {
+            if (associates == null)
+                return BadRequest();
+
+          var result = await _associateProcessor.SaveAssociates(associates);
+          return Ok(result);
+        }
+        
+
+    }
+}
